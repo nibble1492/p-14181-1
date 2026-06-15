5f01f8eb5a1e36f3a9b7094994c91e9a74955a2d
JUNHYEONG LEE <nibble1492@gmail.com>
2026-06-15 15:43:11 +0900

    033

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -1,7 +1,6 @@
 package com.back.domain.post.post.controller;
 
 import com.back.domain.member.member.entity.Member;
-import com.back.domain.member.member.service.MemberService;
 import com.back.domain.post.post.dto.PostDto;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
@@ -15,20 +14,18 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@Validated
 @RequestMapping("/api/v1/posts")
 @RequiredArgsConstructor
 @Tag(name = "ApiV1PostController", description = "API 글 컨트롤러")
 public class ApiV1PostController {
     private final PostService postService;
-    private final MemberService memberService;
-    
+    private final Rq rq;
+
     @GetMapping
     @Transactional(readOnly = true)
     @Operation(summary = "다건 조회")
@@ -50,6 +47,28 @@ public class ApiV1PostController {
         return new PostDto(post);
     }
 
+    @DeleteMapping("/{id}")
+    @Transactional
+    @Operation(summary = "삭제")
+    public RsData<Void> delete(
+            @PathVariable int id
+    ) {
+        Member actor = rq.getActor();
+
+        Post post = postService.findById(id).get();
+
+        if (!actor.equals(post.getAuthor()))
+            throw new ServiceException("403-1", "글 삭제 권한이 없습니다.");
+
+        postService.delete(post);
+
+        return new RsData<>(
+                "200-1",
+                "%d번 글이 삭제되었습니다.".formatted(id)
+        );
+    }
+
+
     record PostWriteReqBody(
             @NotBlank
             @Size(min = 2, max = 100)
@@ -59,15 +78,14 @@ public class ApiV1PostController {
             String content
     ) {
     }
+
     @PostMapping
     @Transactional
     @Operation(summary = "작성")
     public RsData<PostDto> write(
-            @Valid @RequestBody PostWriteReqBody reqBody,
-            @NotBlank @Size(min = 30, max = 50) @RequestHeader("Authorization") String authorization
+            @Valid @RequestBody PostWriteReqBody reqBody
     ) {
-        String apiKey = authorization.replace("Bearer ", "");
-        Member actor = memberService.findByApiKey(apiKey).orElseThrow(() -> new ServiceException("401-1", "존재하지 않는 apiKey 입니다."));
+        Member actor = rq.getActor();
 
         Post post = postService.write(actor, reqBody.title, reqBody.content);
 
@@ -87,54 +105,26 @@ public class ApiV1PostController {
             String content
     ) {
     }
+
     @PutMapping("/{id}")
     @Transactional
     @Operation(summary = "수정")
     public RsData<Void> modify(
             @PathVariable int id,
-            @Valid @RequestBody PostModifyReqBody reqBody,
-            @NotBlank @Size(min = 30, max = 50) @RequestHeader("Authorization") String authorization
+            @Valid @RequestBody PostModifyReqBody reqBody
     ) {
-        String apiKey = authorization.replace("Bearer ", "");
-
-        Member actor = memberService.findByApiKey(apiKey).orElseThrow(() -> new ServiceException("401-1", "존재하지 않는 apiKey 입니다."));
+        Member actor = rq.getActor();
 
         Post post = postService.findById(id).get();
 
-        if (!actor.equals(post.getAuthor())) {
+        if (!actor.equals(post.getAuthor()))
             throw new ServiceException("403-1", "글 수정 권한이 없습니다.");
-        }
 
         postService.modify(post, reqBody.title, reqBody.content);
 
         return new RsData<>(
                 "200-1",
                 "%d번 글이 수정되었습니다.".formatted(post.getId())
-        );
-    }
-
-    @DeleteMapping("/{id}")
-    @Transactional
-    @Operation(summary = "삭제")
-    public RsData<Void> delete(
-            @PathVariable int id,
-            @NotBlank @Size(min = 30, max = 50) @RequestHeader("Authorization") String authorization
-    ) {
-        String apiKey = authorization.replace("Bearer ", "");
-
-        Member actor = memberService.findByApiKey(apiKey).orElseThrow(() -> new ServiceException("401-1", "존재하지 않는 apiKey 입니다."));
-
-        Post post = postService.findById(id).get();
-
-        if (!actor.equals(post.getAuthor())) {
-            throw new ServiceException("403-1", "글 삭제 권한이 없습니다.");
-        }
-
-        postService.delete(post);
-
-        return new RsData<>(
-                "200-1",
-                "%d번 글이 삭제되었습니다.".formatted(id)
         );
     }
 }
