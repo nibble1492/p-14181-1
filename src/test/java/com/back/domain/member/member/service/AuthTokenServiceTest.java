f7f930ae297fc2340f4309553819cb44fd847baa
JUNHYEONG LEE <nibble1492@gmail.com>
2026-06-17 14:15:37 +0900

    072

--- src/test/java/com/back/domain/member/member/service/AuthTokenServiceTest.java
+++ src/test/java/com/back/domain/member/member/service/AuthTokenServiceTest.java
@@ -30,8 +30,8 @@ public class AuthTokenServiceTest {
     @Autowired
     private AuthTokenService authTokenService;
 
-    private int expireSeconds = 60 * 60 * 24 * 365;
-    private String secret = "abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890";
+    private final int expireSeconds = 60 * 60 * 24 * 365;
+    private final String secret = "abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890";
 
     @Test
     @DisplayName("authTokenService 서비스가 존재한다.")
@@ -40,7 +40,7 @@ public class AuthTokenServiceTest {
     }
 
     @Test
-    @DisplayName("jjwt 최신 방식으로 JWT 생성, {name=\"Paul\", age=23}")
+    @DisplayName("jjwt 최신 방식으로 JWT 생성, {name=\"Paul\", age=23}, 그리고 payload 추출")
     void t2() {
         // 토큰 만료기간: 1년
         long expireMillis = 1000L * expireSeconds;
@@ -65,8 +65,6 @@ public class AuthTokenServiceTest {
                 .compact();
 
         assertThat(jwt).isNotBlank();
-
-        System.out.println("jwt = " + jwt);
 
         // 키가 유효한지 테스트
         Claims claims = Jwts.parser()
@@ -114,5 +112,15 @@ public class AuthTokenServiceTest {
         assertThat(accessToken).isNotBlank();
 
         System.out.println("accessToken = " + accessToken);
+
+        Map<String, Object> parsedPayload = authTokenService.payload(accessToken);
+
+        assertThat(parsedPayload)
+                .containsAllEntriesOf(
+                        Map.of(
+                                "id", memberUser1.getId(),
+                                "username", memberUser1.getUsername()
+                        )
+                );
     }
 }
